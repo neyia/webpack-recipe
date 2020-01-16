9f913bf8dec4cc34a14dba63e9655723dfd5cca1
nzhyrkova <neyia@mail.ru>
2020-01-16 17:45:22 +0300

    Adding axios npm package. View recipes at the console.log

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import styles from './../styles/styles.css';
-
+import axios from 'axios'
 import SearchView from './view/SearchView';
 
 function requireAll(r) {
@@ -9,3 +9,13 @@ function requireAll(r) {
 requireAll(require.context('./../icons/', true, /\.svg$/));
 
 console.log(SearchView);
+
+async function getResults(query) {
+    const ID = '0b4f11dd';
+    const KEY = '51a2d8bad09a42a5663f92fe7d234c29';
+    const PROXY = '';
+    const res = await axios(`${PROXY}https://api.edamam.com/search?app_key=${KEY}app_id=${ID}&q=${query}`);
+    console.log(res.data.hits);
+}
+
+getResults('pizza');
