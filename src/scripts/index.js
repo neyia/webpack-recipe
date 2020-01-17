81211cec193db65dee4b52af312b128cdb4f3f1e
nzhyrkova <neyia@mail.ru>
2020-01-17 17:03:59 +0300

    Adding models -> classes (142-2.07)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,17 @@
 import styles from './../styles/styles.css';
-import axios from 'axios'
 import SearchView from './view/SearchView';
+import Search from './models/Search';
 
 function requireAll(r) {
     r.keys().forEach(r);
 }
-
 requireAll(require.context('./../icons/', true, /\.svg$/));
 
-console.log(SearchView);
+//the global state
 
-async function getResults(query) {
-    const ID = '0b4f11dd';
-    const KEY = '51a2d8bad09a42a5663f92fe7d234c29';
-    const PROXY = '';
+const state = {};
 
-    try{
-        const res = await axios(`${PROXY}https://api.edamam.com/search?app_key=${KEY}app_id=${ID}&q=${query}`);
-        const result = res.data.hits;
-        console.log(result);
-    }
-    catch (error) {
-        alert(error);
-    }
 
-}
-
-getResults('tomato pasta');
-
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
